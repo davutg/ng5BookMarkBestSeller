abc44acc6a42bcd7156ecc3a0d56039ceb4f412f
Davut Gurbuz <davutgurbuz@gmail.com>
2018-01-24 09:38:22 +0300

    cart service remove item

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -9,7 +9,9 @@ import { BsGlobalService } from './bs-global.service';
 export class CartService {
 
   private items = new ReplaySubject<Array<ProductModel>>(1);;
+  removeItem = new ReplaySubject<ProductModel>(1);;
   shoppingList=this.items.asObservable();
+
   constructor(private _cookies:CookieService) { 
 
     this.loadFromCache();
@@ -56,7 +58,7 @@ export class CartService {
     console.log(wada+" removing ...");
     var cartItems=new Array<ProductModel>();
     var cartCache=this._cookies.get('shoppingList')
-
+    this.removeItem.next(wada);
     if(cartCache!="")    
     {      
       cartItems = (JSON.parse(cartCache) as Array<ProductModel>);
